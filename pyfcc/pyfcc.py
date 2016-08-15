c7580d383de08bc72657e49c4d901b18ece76f62
abevieiramota <abevieiramota@gmail.com>
2016-08-15 15:02:57 -0300

    avançando(ou não) com a ideia de extrair o texto formatado com pdf2htmlEX

--- pyfcc/pyfcc.py
+++ pyfcc/pyfcc.py
@@ -1,57 +1,111 @@
 import re
 from collections import namedtuple
+from collections import defaultdict
 from bs4 import BeautifulSoup
-
-ini_questao = re.compile(r'^(\d{1,2})\.')
-ini_item = re.compile(r'^\(([A-E])\)')
-end_item_E = re.compile(r'Caderno de Prova')
-
-soup = BeautifulSoup(open('pdf2htmlEX/2016-trt-' +
-                          '14-regiao-ro-e-ac-analista-judiciario-tecnologia' +
-                          '-da-informacao.html'), 'html.parser')
 
 Questao = namedtuple('Questao', 'enunciado')
 
 
 class Prova:
 
+    ini_questao_re = re.compile(r'^(\d{1,2})\.')
+    end_questao_re = re.compile(r'Caderno de Prova')
+    ini_item_re = re.compile(r'^\(([A-E])\)')
+    rank_symbol_re = re.compile(r'(?<=\d)o')
+    enunciado_questao_re = re.compile(r'^(\d{1,2})\.\s*(.*)', re.DOTALL)
+    spaces_to_tab_re = re.compile(r'\s{2,}')
+    begin_sentence_re = re.compile(r'^[A-Z]')
+
     def __init__(self, filepath):
+
+        self.divs_questoes = {}
+        self._collect_divs(filepath)
+
+    def _ini_questao_match(self, content):
+
+        return Prova.ini_questao_re.match(content)
+
+    def _end_questao_match(self, content):
+
+        return Prova.end_questao_re.match(content)
+
+    def _ini_item_match(self, content):
+
+        return Prova.ini_item_re.match(content)
+
+    def _collect_divs(self, filepath):
 
         with open(filepath) as prova:
 
             soup = BeautifulSoup(prova, 'html.parser')
 
-            self.divs_questoes = {}
-
             questao = None
-            pos_item = False
+            parte = None
 
             for div in soup.find_all('div'):
 
-                is_ini_questao = ini_questao.match(div.get_text())
+                div_text = div.get_text().strip()
 
-                if is_ini_questao:
+                ini_questao_match = self._ini_questao_match(div_text)
+                end_questao_match = self._end_questao_match(div_text)
+                ini_item_match = self._ini_item_match(div_text)
 
-                    questao = is_ini_questao.group(1)
-                    self.divs_questoes[questao] = []
-                    pos_item = False
+                if ini_questao_match:
 
-                if ini_item.match(div.get_text()):
+                    questao = int(ini_questao_match.group(1))
+                    self.divs_questoes[questao] = defaultdict(list)
+                    parte = 'enunciado'
 
-                    pos_item = True
+                elif ini_item_match:
 
-                if not div.get_text().strip() and pos_item:
+                    parte = ini_item_match.group(1)
+
+                elif end_questao_match:
 
                     questao = None
+                    parte = None
 
-                if questao:
+                if parte:
 
-                    self.divs_questoes[questao].append(div)
+                    self.divs_questoes[questao][parte].append(div)
+
+    def _get_div_text(self, div):
+
+        div_content = div.get_text()
+
+        div_span = div.find('span')
+
+        if div_span and 'v5' in div_span['class']:
+
+            return '\n\n' + div_content.strip()
+
+        if not div_content.strip():
+
+            return ' '
+
+        if 'ffa' in div['class']:
+
+            div_content = Prova.spaces_to_tab_re.sub('\t', div_content.strip())
+
+            return '\n\n' + div_content.rstrip()
+
+        if not Prova.begin_sentence_re.match(div_content):
+
+            return ' ' + div_content
+
+        return div_content
+
+    def _get_enunciado(self, i):
+
+        divs_enunciado = self.divs_questoes[i]['enunciado']
+
+        full_enunciado = ''.join((self._get_div_text(div)
+                                 for div in divs_enunciado)).strip()
+
+        full_enunciado = Prova.rank_symbol_re.sub('º', full_enunciado)
+
+        return Prova.enunciado_questao_re.match(full_enunciado).group(2)
 
     def __getitem__(self, i):
 
-        return Questao(enunciado=self.divs_questoes[str(i)][0].get_text())
-
-        # return Questao(enunciado='Entre os graves equívocos que podem'
-        #                          ' se incluir na relação entre um homem'
-        #                          ' e uma mulher destaca-se, no texto,')
+        return Questao(enunciado=self._get_enunciado(i))
--- pyfcc/test_pyfcc.py
+++ pyfcc/test_pyfcc.py
@@ -1,17 +1,65 @@
 # -*- coding: utf-8 -*-
 import pyfcc
 
+prova = pyfcc.Prova('pdf2htmlEX/'
+                    '2016-trt-14-regiao-ro-e-ac-analista-judiciario-'
+                    'tecnologia-da-informacao.html')
 
-def test_trt_2016():
 
-    prova = pyfcc.Prova('pdf2htmlEX/'
-                        '2016-trt-14-regiao-ro-e-ac-analista-judiciario-'
-                        'tecnologia-da-informacao.html')
+def test_trt_2016_q1():
 
-    assert prova[1].enunciado == ('Entre os graves equívocos que podem'
+    assert prova[1].enunciado == (r'Entre os graves equívocos que podem'
                                   ' se incluir na relação entre um homem'
                                   ' e uma mulher destaca-se, no texto,')
+
+
+def test_trt_2016_q2():
 
     assert prova[4].enunciado == ('Considerando-se o contexto,'
                                   ' traduz-se adequadamente o sentido'
                                   ' de um segmento em:')
+
+
+def test_trt_2016_q3():
+
+    assert prova[16].enunciado == ('Um comerciante compra certa mercadoria'
+                                   ' por R$ 149,50 e estabelece o preço de '
+                                   'venda levando em consideração que ele quer'
+                                   ' obter um lucro de 20% sobre o preço de '
+                                   'venda, e que ele deverá pagar um imposto '
+                                   'de 15% sobre o mesmo preço de venda.'
+                                   ' Nas condições dadas, o preço de venda da '
+                                   'mercadoria deverá ser, em R$, de')
+
+
+def test_trt_2016_q20():
+
+    enunciado = ('Observe os sete primeiros termos de uma sequência numérica:'
+                 ' 7, 13, 25, 49, 97, 193, 385, ... . Mantido o mesmo padrão '
+                 'da sequência e admitindo-se que o 100º termo seja igual a x,'
+                 ' então o 99º termo dela será igual a')
+
+    assert prova[20].enunciado == enunciado
+
+
+def test_trt_2016_q22():
+
+    enunciado = ('Em um computador que utiliza o Sistema Operacional Linux, '
+                 'um Analista digitou um comando e foram mostrados os dados'
+                 ' abaixo. '
+                 '\n\n'
+                 'Active Internet connections (servers and established)'
+                 '\n\n'
+                 'Proto\tRecv-Q\tSend-Q\tLocal Address\tForeign Address\tState'
+                 '\n'
+                 'tcp\t0\t0\tlocalhost:30037\t*:*\tLISTEN'
+                 '\n'
+                 'tcp\t0\t0\tlocalhost:ipp\t*:*\tLISTEN'
+                 '\n'
+                 'tcp\t0\t0\t*:smtp\t*:*\tLISTEN'
+                 '\n'
+                 'tcp6\t0\t0\tlocalhost:ipp\t[::]:*\tLISTEN'
+                 '\n\n'
+                 'O comando digitado foi')
+
+    assert prova[22].enunciado == enunciado
